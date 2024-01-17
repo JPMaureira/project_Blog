55fba5a2c2a3cefd8558d0ac6a701b48163ec74a
Juan Pablo Maureira <juanpablomaureira.r@gmail.com>
2024-01-16 22:40:35 -0300

    Paso a produccion 1

---
+++ project_Blog/models.py
@@ -0,0 +1,50 @@
+# # models.py
+# from django.contrib.auth.models import User
+# from django.contrib.auth.models import AbstractUser, Group, Permission
+# from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission, Group
+# from django.db import models
+
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission, Group
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+class UsuarioPersonalizadoManager(BaseUserManager):
+    def create_user(self, email, password=None, nombre=None, **extra_fields):
+        if not email:
+            raise ValueError('El campo Email debe ser establecido')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, nombre=nombre, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, nombre=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, nombre, **extra_fields)
+
+class UsuarioPersonalizado(AbstractUser):
+    nombre = models.CharField(max_length=30, blank=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=20, blank=True)
+
+    objects = UsuarioPersonalizadoManager()
+
+    groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, verbose_name='user permissions', blank=True, related_name='custom_user_permissions')
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='page_images/')
+    post_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
---
+++ project_Blog/views.py
@@ -0,0 +1,138 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.http import HttpResponse
+
+from django.contrib.auth import login, authenticate
+from django.template import Template, Context
+from .models import UsuarioPersonalizado
+from django.views.decorators.csrf import csrf_protect
+from django.db.models import Q
+
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm,PageForm
+from .models import Page
+from .forms import BuscarEventoForm
+
+from django.contrib.auth.decorators import login_required
+
+
+from django.contrib import messages
+
+# Vista de registro
+def register(request):
+
+      if request.method == 'POST':
+
+            #form = UserCreationForm(request.POST)
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+
+                  username = form.cleaned_data['username']
+                  form.save()
+                  return render(request,"panel.html" ,  {"mensaje":"Usuario Creado :)"})
+
+      else:
+            #form = UserCreationForm()       
+            form = UserRegisterForm()     
+
+      return render(request,"registro.html" ,  {"form":form})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            contrasenia = form.cleaned_data.get('password')
+
+            user = authenticate(username=usuario, password=contrasenia)
+
+            if user is not None:
+                login(request, user)
+                return redirect('panel')  # Redirige a la vista panel
+            else:
+                return render(request, "home.html", {"mensaje": "Datos incorrectos"})
+        else:
+            return render(request, "home.html", {"mensaje": "Formulario erroneo"})
+
+    form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+@login_required  # Este decorador asegura que solo los usuarios autenticados puedan acceder a la vista
+def panel(request):
+   
+    usuario = request.user
+    context = {"usuario": usuario}
+    return render(request, "panel.html", context)
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def pages(request):
+    return render(request, 'pages.html')
+
+@login_required
+def create_page(request):
+    if request.method == 'POST':
+        form = PageForm(request.POST, request.FILES)
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.author = request.user
+            page.save()
+            messages.success(request, 'Page created successfully!')
+            return redirect('page_creada')
+    else:
+        form = PageForm()
+    return render(request, 'create_page.html', {'form': form})
+
+@login_required
+def page_creada(request):
+     
+    messages_to_show = request.session.get('messages_to_show', [])
+
+    request.session['messages_to_show'] = []
+    
+    pages = Page.objects.all()
+
+    form_buscar = BuscarEventoForm(request.GET)
+    if form_buscar.is_valid():
+        busqueda = form_buscar.cleaned_data['busqueda']
+        pages = pages.filter(Q(title__icontains=busqueda) | Q(author__icontains=busqueda))
+
+    return render(request, 'page_creada.html', {'pages': pages, 'messages_to_show': messages_to_show, 'form_buscar': form_buscar})
+
+
+def eliminar_page(request, page_id):
+    page = Page.objects.get(pk=page_id)
+    page.delete()
+    messages.success(request, 'Page eliminado exitosamente.')
+    return redirect('page_creada')
+
+
+def editar_page(request, page_id):
+    page = get_object_or_404(Page, pk=page_id)
+
+    if request.method == 'POST':
+        form = PageForm(request.POST, instance=page)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Page editado exitosamente.')
+            return redirect('page_creada')
+    else:
+        form = PageForm(instance=page)
+
+    return render(request, 'editar_page.html', {'form': form, 'page_id': page_id})
